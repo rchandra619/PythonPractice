6aa8a84c39d5ca2beaecb35f77f47703369535ae
ravichandra <ravichandrarc494@gmail.com>
2022-04-05 16:02:05 +0530

    Second Commit from Pycharm

--- Pyhton Files/FromGitHub.py
+++ Pyhton Files/FromGitHub.py
@@ -1,4 +1,14 @@
+import numpy as np
+
 var = 'helloworld'
 print(var)
+
+diabates = np.genfromtxt('E:/Captures/Datasets/diabetes.csv',delimiter=',',skip_header=0)
+
+print(diabates[0,:])
+
+
+
+print("Hello world from git hub")
 
 print("Hello world from git hub")
